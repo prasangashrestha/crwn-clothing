05ad36f5c350fd00b30fc69bfb6caabe71b6da0a
Prasanga <101890972@student.swin.edu.au>
2020-04-29 07:09:42 +1000

    adding header component to the pages

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item'
 
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
                 {
-                    items.splice(1,5).map(item => (
-                        <div key={item.id}>{item.name}</div>
+                    items
+                    .filter((item,index) => index<4)
+                    .map(({id, ...otherProps}) => (
+                        <CollectionItem key={id} {...otherProps} />
                     ))
                 }
             </div>
