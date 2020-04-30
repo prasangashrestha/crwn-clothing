1bf57858240e7e725aa12c73781c0a44624da372
Prasanga <101890972@student.swin.edu.au>
2020-04-30 20:25:37 +1000

    implemented firebase utils, included ability to share authenticated users in database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shop/shop';
 import SignInAndSignUp from './pages/singin-signup/signin-signup';
 import Header from './components/header/header';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 
@@ -23,9 +23,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await  createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, () => console.log(this.state))
+        })
+        
+      }else{
+        this.setState({currentUser: userAuth})
+      }
+     
     })
+    
   }
 
   componentWillUnmount(){
--- src/pages/singin-signup/signin-signup.js
+++ src/pages/singin-signup/signin-signup.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './signin-signup.styles.scss'
 import SignIn from '../../components/sign-in/sign-in'
+import SignUp from '../../components/sign-up/sign-up'
 
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         
         <SignIn />
+        <SignUp />
         
     </div>
 )
