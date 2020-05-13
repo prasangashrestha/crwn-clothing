55535c0548693aacc1c37e65e98798c9334af20f
Prasanga <101890972@student.swin.edu.au>
2020-05-14 07:01:57 +1000

    Updated navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,24 +13,24 @@ import CartDropdown from '../card-dropdown/card-dropdown';
 const Header = ({currentUser, hidden}) => {
     return (
         <div className="header">
-            <Link className= 'logo-container' to= "/">
+            <Link className= 'logo-container' to= "/crwn-clothing">
                 <Logo className='Logo' />
             </Link>
        
 
             <div className="options">
-                <Link className="option" to="/shop">
+                <Link className="option" to="/crwn-clothing/shop">
                     SHOP
                 </Link>
 
-                <Link className="option" to="/contact">
+                <Link className="option" to="/crwn-clothing/contact">
                     CONTACT
                 </Link>
                 {
                     currentUser?
                     <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                     :
-                    <Link className='option' to="/signin">SIGN IN</Link>
+                    <Link className='option' to="/crwn-clothing/signin">SIGN IN</Link>
                 }
                 <CartIcon />
             </div>
