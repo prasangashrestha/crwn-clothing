0bb87137869844dd7d77d7c590129e28dbf9f602
Prasanga <101890972@student.swin.edu.au>
2020-05-14 06:47:05 +1000

    Updates

---
+++ precache-manifest.c7f4c2fde0156499facfb4b6dea91c18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3b8e5a16f17b2628a8ce8299786226",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "5130aa04c52abddd69fe",
+    "url": "/crwn-clothing/static/css/main.3c5cace7.chunk.css"
+  },
+  {
+    "revision": "9bc66ec4651b7cfca718",
+    "url": "/crwn-clothing/static/js/2.f9dc469b.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/crwn-clothing/static/js/2.f9dc469b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5130aa04c52abddd69fe",
+    "url": "/crwn-clothing/static/js/main.bc8d401b.chunk.js"
+  },
+  {
+    "revision": "2756d63e95ecf2342c4a",
+    "url": "/crwn-clothing/static/js/runtime-main.1b9cad0d.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
