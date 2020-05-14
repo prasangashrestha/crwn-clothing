7d602430a2fd5d5644c40d9da96f489e4979e77c
Prasanga <101890972@student.swin.edu.au>
2020-05-14 12:20:39 +1000

    Added Redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,15 @@
 import { combineReducers } from 'redux';
+import { persistRducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
 
 export default combineReducers({
     user: userReducer,
